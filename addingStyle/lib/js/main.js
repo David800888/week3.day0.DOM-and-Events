9e1e7ba1f2754adb57a79c7f29b8ba2211308fd9
David <davca2322@gmail.com>
2021-08-02 21:06:15 -0400

    save_changes

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,8 @@
+let right_side = document.querySelector("div.right_side");
+right_side.style["background-color"] = "#d3c26f";
+
+let names = document.getElementsByClassName("name");
+for (let index = 0; index < names.length; index++) {
+    names[index].style["text-shadow"] = "-6px 5px 2px yellow";
+    
+}
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,8 @@
+alert("You've been deleted");
+
+window.onclick = function() {
+    document.getElementById("rengar").src = "assets/rengar.png"
+    
+}
+onClickListener
+btn.setOnClickListener(myOnClickListener);
